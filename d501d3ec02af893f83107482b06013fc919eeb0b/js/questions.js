09785a9ff5ae613f72416cf8eea1407cd8059447
pharmacythings <thewhiterabbit@riseup.net>
2026-04-23 16:26:59 +0100

    quiz

---
+++ d501d3ec02af893f83107482b06013fc919eeb0b/js/questions.js
@@ -0,0 +1,80 @@
+const myQuestions = [
+  {
+    title: "Question 1 - OTC",
+    question: "What does OTC stand for?...<p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide2.jpg'>",
+    answers: {
+      a: "Off the cuff",
+      b: "Over the counter",
+      c: "Occupational Therapy Classes."
+    },
+    correctAnswer: "b",
+    correctAnswerText: "That is correct.<p>OTC stands for “over-the-counter,” (non-prescription) medicine. These are medicines available without a healthcare provider’s prescription.</p>",
+    falseAnswerText: "That's not correct.<p>OTC stands for “over-the-counter,” (non-prescription) medicine. These are medicines available without a healthcare provider’s prescription.</p>"
+  },
+  {
+    title: "Question 2 - Saxenda",
+    question: "How long will a Saxenda pen last when taking 'full dose'?<p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide1.jpg'>",
+    answers: {
+      a: "24 hours",
+      b: "6 days",
+      c: "30 days"
+    },
+    correctAnswer: "b",
+    correctAnswerText: "That is correct.<p>Once you are taking the 3-mg dose, each pen will last 6 days.</p>",
+    falseAnswerText: "That's not correct.<p>Once you are taking the 3-mg dose, each pen will last 6 days.</p>"
+  },
+  {
+    title: "Question 3 - Royal Mail",
+    question: "What is the maximum weight allowed for a Royal Mail package?</p><p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide3.jpg'>",
+    answers: {
+      a: "2kg.",
+      b: "5kg",
+      c: "10kg"
+    },
+    correctAnswer: "a",
+    correctAnswerText: "That is correct.<p>Anything above 2kg should be posted using DPD</p>",
+    falseAnswerText: "That´s wrong!<p>Anything above 2kg should be posted using DPD.</p>"
+  },
+  {
+    title: "Question 4 - Rubbish",
+    question: "You have been packing medicines and now have a stack of patient invoices. How do you dispose of them?<p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide4.jpg'>",
+    answers: {
+      a: "In the 'confidential waste' sack",
+      b: "In the 'general waste' bin",
+      c: "Set fire to them"
+    },
+    correctAnswer: "a",
+    correctAnswerText: "That is correct.<p>Anything that could identify a patient should be disposed of in the confidential waste.</p>",
+    falseAnswerText: "That is not correct.<p>Anything that could identify a patient should be disposed of in the confidential waste.</p>"
+  },
+  {
+    title: "Question 5 - Royal Mail (again!)",
+    question: "What is the maximum dimensions of a parcel you can send via Royal Mail as a 'small parcel'?<p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/imageP.jpg'>",
+    answers: {
+      a: "50cm x 30cm x 14cm",
+      b: "55cm x 40cm x 12cm",
+      c: "45cm x 35cm x 16cm"
+    },
+    correctAnswer: "c",
+    correctAnswerText: "That is correct.<p>Anything larger than '45cm x 35cm x 16cm' has to be sent either DPD, DX or Parcel Force.</p>",
+    falseAnswerText: "That is not correct.<p>Anything larger than '45cm x 35cm x 16cm' has to be sent either DPD, DX or Parcel Force..</p>"
+  },
+  {
+    title: "Question 6 - Fire alarm",
+    question: "In case of fire. Raise the Alarm & warn people!<p> Fire alarm break-glass points are located in different areas of the building. Do you know the location of the nearest fire alarm in the dispensary?</p><p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide5.jpg'>",
+    answers: {
+      a: "Main entrance door",
+      b: "Beside the Fire Exit",
+      c: "Opposite the toilets"
+    },
+    correctAnswer: "b",
+    correctAnswerText: "That is correct.<p>The nearest fire alarm is located beside the top Fire Exit.</p>",
+    falseAnswerText: "That is not correct.<p>Perhaps now would be a good time to find out?!.</p>"
+  }
+];
